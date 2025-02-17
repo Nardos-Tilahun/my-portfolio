8ebc842fc0135b1c4f8bb94ca6c2be810e5e276d
Nardos-Tilahun <contactnardos@gmail.com>
2025-02-17 19:31:22 +0300

    resume added

--- app/page.tsx
+++ app/page.tsx
@@ -112,8 +112,23 @@ export default function Home() {
                 <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.26.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.084-.729.084-.729 1.205.084 1.84 1.236 1.84 1.236 1.07 1.834 2.809 1.304 3.495.997.108-.776.419-1.305.763-1.605-2.665-.303-5.466-1.332-5.466-5.93 0-1.311.469-2.382 1.236-3.221-.123-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.292-1.552 3.297-1.23 3.297-1.23.653 1.653.241 2.874.118 3.176.77.839 1.236 1.91 1.236 3.221 0 4.61-2.804 5.625-5.474 5.922.43.371.823 1.102.823 2.221v3.293c0 .32.192.694.801.576 4.765-1.587 8.201-6.085 8.201-11.386 0-6.627-5.373-12-12-12z" />
               </svg>
             </a>
+            <a
+              href="https://drive.google.com/file/d/1P_YYCpsCBeyFPRgElSP_qPysTG846Ut_/view?usp=drive_link"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-green-400 hover:text-green-300 transition-transform transform hover:scale-110"
+              aria-label="Resume"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zm4 18H6V4h7v5h5v11zM8 15h8v2H8v-2zm0-4h8v2H8v-2z" />
+              </svg>
+            </a>
           </div>
-
           {/* Right Section */}
           <div className="text-center md:text-right">
             <p className="text-sm leading-loose text-green-400/80">
