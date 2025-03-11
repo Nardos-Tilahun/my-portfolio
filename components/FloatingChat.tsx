6e2790bedf0f98b6e9c3d1aeb8e4ba12efaef58a
Nardos-Tilahun <contactnardos@gmail.com>
2025-03-11 16:29:32 +0300

    chat bot ammend for small screen

--- components/FloatingChat.tsx
+++ components/FloatingChat.tsx
@@ -315,7 +315,7 @@ export default function FloatingChat() {
   )
 
   return (
-    <div className="fixed bottom-16 right-6 z-50">
+    <div className="fixed bottom-16 right-1 xs:right-6 z-50">
       <AnimatePresence>
         {isOpen ? (
           <motion.div
