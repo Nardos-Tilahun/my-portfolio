1df9f26f9a1e8f0ab2c6973f085ac99256c2d816
Nardos T. Dubale <90634719+Nardos-Tilahun@users.noreply.github.com>
2025-07-20 17:27:27 +0300

    Change in Cloudinary CV resume link.tsx

--- data/PersonalInfo.tsx
+++ data/PersonalInfo.tsx
@@ -272,7 +272,7 @@ export const personalData = {
     "linkedin": "www.linkedin.com/in/nardosdubale",
     "github": "https://github.com/Nardos-Tilahun",
     "twitter": "@Nardos_Tilahun",
-    "resume": "https://res.cloudinary.com/dyayxqlzr/image/upload/v1753019590/Nardos_Tilahun_CV_Resume_wumtqa.pdf"
+    "resume": "https://res.cloudinary.com/dyayxqlzr/image/upload/v1753021337/Nardos_Tilahun_CV_Resume_djluwf.pdf"
   },
   "interests": [
     "Web development",
